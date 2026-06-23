f518e0cf6dcee3cbb50d1c10ab0baacbd3880de7
Vimalraj <Vimalrajragupathi@gmail.com>
2026-06-23 16:27:04 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,75 @@
+// Task 1 - Employee Name
+
+let employeename=prompt("Employee Name")
+console.log("Employee Name:",employeename)
+document.writeln("Employee Name:" + employeename + "<br>")
+
+// Task 2 - Company Entry Confirmation
+
+let box=confirm("Are you ready to join today's JavaScript training?")
+console.log(box)
+
+// Task 3 - Customer Greeting
+
+let Customer=alert("Welcome to Stackly Solutions!")
+console.log(Customer)
+
+// Task 4 - Student Details
+
+let Studentname=prompt("Student name")
+console.log("Name",Studentname)
+let Studentage=prompt("Student age")
+console.log("Age",Studentage)
+
+// Task 5 - Product Price
+
+let productPrice=500
+productPrice=600
+console.log(productPrice)
+
+// Task 6 - Login Validation
+
+let Username=prompt("Username")
+
+if (Username==""){
+    console.warn("Warning: Username is empty.")
+}
+else {
+    console.log("login successful",Username)
+}
+
+// Task 7 - Website Maintenance
+
+alert("Website is under maintenance.")
+console.error("Website is under maintenance.")
+
+// Task 8 - Feedback Collection
+
+let feedback=prompt("How was today's JavaScript session?")
+console.log(feedback)
+document.writeln("Thank you for your feedback!")
+
+// Task 9 - Profile Information
+
+let name=prompt("name")
+let city=prompt("city")
+let language=prompt("Favorite Programming Language")
+console.log("your name:",name)
+console.log("city:",city)
+console.log("Favorite Programming Language:",language)
+
+// Task 10 - Mini Registration Form
+
+console.log("Registration Successful!");
+   
+console.log("Registration Details")
+let fullName=prompt("Full Name")
+let Email=prompt("Email")
+let mobilenumber=prompt("Mobile Number")
+console.log("Name   :",fullName)
+console.log("Email  :",Email)
+console.log("Mobile :",mobilenumber)
+
+let mess=alert("Registration Successful!")
+console.log(mess)
+
